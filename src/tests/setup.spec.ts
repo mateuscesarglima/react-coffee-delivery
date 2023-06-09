d00ab355ca0d6bfbab8518e387fb6245cb033eef
Mateus Cesar <mateuscesarglima@gmail.com>
2023-06-09 01:21:05 -0300

    feat(COFFEE-2): Add configs
    -  Jest config
    -  Path aliases config
    -  Eslint config
    -  Prettier config

---
+++ src/tests/setup.spec.ts
@@ -0,0 +1,5 @@
+describe('teste', () => {
+  it('should pass', () => {
+    expect(1 + 2).toBe(3)
+  })
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,53 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: resolve(__dirname, './src') },
+      {
+        find: '@Components',
+        replacement: resolve(__dirname, './src/components'),
+      },
+      {
+        find: '@Pages',
+        replacement: resolve(__dirname, './src/pages'),
+      },
+      {
+        find: '@Routes',
+        replacement: resolve(__dirname, './src/routes'),
+      },
+      {
+        find: '@Layouts',
+        replacement: resolve(__dirname, './src/layouts'),
+      },
+      {
+        find: '@Assets',
+        replacement: resolve(__dirname, './src/assets'),
+      },
+      {
+        find: '@Constants',
+        replacement: resolve(__dirname, './src/constants'),
+      },
+      {
+        find: '@Context',
+        replacement: resolve(__dirname, './src/context'),
+      },
+      {
+        find: '@Types',
+        replacement: resolve(__dirname, './src/types'),
+      },
+      {
+        find: '@Reducers',
+        replacement: resolve(__dirname, './src/reducers'),
+      },
+      {
+        find: '@Hooks',
+        replacement: resolve(__dirname, './src/hooks'),
+      },
+    ],
+  },
 })
