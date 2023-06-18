0ced5d41b60fb4028cae99580e53db3f71cc4c05
Mateus Cesar <mateuscesarglima@gmail.com>
2023-06-18 16:16:39 -0300

    feat(COFFEE-6): Update continuous integration config

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-import { useState } from 'react'
-
 export const App = () => {
-  const arr = ['mateus', 'felipe', 'rodrigo']
-  const [showText, setShowText] = useState(false)
   return (
     <div>
-      <div>
-        {arr.map((obj) => (
-          <li key={obj}>{obj}</li>
-        ))}
-      </div>
-      <button onClick={() => setShowText(!showText)}>Show text</button>
-      {showText && <p>Apareci</p>}
+      <p>Testando</p>
+      <p>nao sei</p>
     </div>
   )
 }
